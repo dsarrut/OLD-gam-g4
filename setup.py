1e21a318dac36ef59213ba5d716fff5604a924ed
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2020-09-28 17:27:02 +0200

    Towards v0.0.3

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     # FIXME --> DO NOT USE YET
 
     name='gam_g4',
-    version='0.0.2',
+    version='0.0.3',
     author='Opengate Collaboration',
     author_email='david.sarrut@creatis.insa-lyon.fr',
     description='A test project using pybind11 and CMake',
